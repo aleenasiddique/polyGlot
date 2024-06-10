bef0639792f7f98af806b090a2461ac555c2df73
aleenasiddique <aleenasiddique3@gmail.com>
2024-06-10 20:28:52 +0500

    comments

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -1,4 +1,5 @@
 export default function TypingAnimation () {
+    //typing animation
     return (
         <div className="flex justify-center gap-2">
             <div className="w-2 h-2 rounded-full bg-white animate-pulse "></div>
